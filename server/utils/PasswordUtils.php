4f3b6bacd8e8e0ff14f73b4b3403089d23824f63
cesar_002 <soul.unleashed13@gmail.com>
2018-11-30 17:46:42 -0600

    se termino en un 98% la pagina de login

---
+++ server/utils/PasswordUtils.php
@@ -0,0 +1,12 @@
+<?php 
+class PasswordUtils {
+    
+    public static function createHash($pass){
+        return password_hash($pass, PASSWORD_BCRYPT);
+    }
+
+    public static function verify($pass, $hash){
+        return password_verify($pass, $hash);
+    }
+
+}
